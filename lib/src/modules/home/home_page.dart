fd047c7de0a196ab9a3bbc2c22255bfc246e8a84
Bruno Rafael <bruno_rafael_08@hotmail.com>
2023-05-09 14:14:36 -0300

    payment, login, menu, global, dio

--- lib/src/modules/home/home_page.dart
+++ lib/src/modules/home/home_page.dart
@@ -1,12 +1,8 @@
 import 'package:flutter/material.dart';
 
-import '../../core/env/env.dart';
 import '../../core/ui/helpers/loader.dart';
 import '../../core/ui/helpers/messages.dart';
-import '../../core/ui/helpers/size_extensions.dart';
-import '../../core/ui/styles/app_styles.dart';
-import '../../core/ui/styles/colors_app.dart';
-import '../../core/ui/styles/text_styles.dart';
+import '../teamplate/base_layout.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -18,41 +14,26 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> with Loader, Messages {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home Page'),
-      ),
-      body: Container(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Padding(
-              padding:const  EdgeInsets.all(8),
-              child: Form(
-                autovalidateMode: AutovalidateMode.onUserInteraction,
-                child: TextFormField(
-                  decoration: const InputDecoration(
-                    label: Text('Login')
-                  ),
-                  validator: (value) => 'error',
+    return Container(
+      child: Column(
+        children: [
+          Padding(
+            padding:const  EdgeInsets.all(8),
+            child: Form(
+              autovalidateMode: AutovalidateMode.onUserInteraction,
+              child: TextFormField(
+                decoration: const InputDecoration(
+                  label: Text('Login'),
                 ),
+                validator: (value) => 'error',
               ),
             ),
-            ElevatedButton(
-              onPressed: () {},
-              child: const Text('Botão'),
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          showLoader();
-          await Future.delayed(const Duration(seconds: 4));
-          hideLoader();
-
-          showError('Erro de alguma coisa');
-        },
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Botão'),
+          ),
+        ],
       ),
     );
   }
